d48cda669c3baeb62fbe282dce65067101090435
Zinovkin Artem <0968064971@gmail.com>
2023-04-15 14:13:52 +0300

    fixed bugs

--- homework_3.py
+++ homework_3.py
@@ -1,7 +1,8 @@
 from pympler import asizeof
 
-def find_a_match(file: str, word: str)-> GeneratorExit:
-    with open(file, "r", encoding= "utf-8") as file_1:
+
+def find_a_match(file: str, word: str) -> GeneratorExit:
+    with open(file, "r", encoding="utf-8") as file_1:
         while True:
             line = file_1.readline()
             if not line:
@@ -9,18 +10,23 @@ def find_a_match(file: str, word: str)-> GeneratorExit:
             if word.lower() in line:
                 yield line
 
-def write_down(match_data: list)-> None:
+
+def write_down(match_data: list) -> None:
     with open("results.txt", "w", encoding="utf-8") as file:
         file.writelines(match_data)
 
+
 def main():
     file_name = "rockyou.txt"
-    input_user = input("enter a search term ") 
+    input_user = input("enter a search term ")
     data = find_a_match(file_name, input_user)
     result = list(data)
     write_down(result)
-    print (f" in the file {len(result)} lines, syze {asizeof.asizeof('results.txt')} bytes")
-    
+    print(
+        f" in the file {len(result)} lines,\
+        syze {asizeof.asizeof('results.txt')} bytes"
+    )
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
